3650cce710121bb9e33aa8b9fbde2b8d94c0bd5f
karanagarwal17 <karanagarwal1702@gmail.com>
2017-10-03 13:35:19 +0530

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header />
         {this.props.children}
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
-import App from './components/App';
-import Home from './components/Home';
+import App from './components/App'
+import Home from './components/Home'
+import Register from './components/Register'
 
 ReactDOM.render((
     <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
+        <Route path="register" component={Register} />
       </Route>
     </Router>
-), document.getElementById('root'));
+), document.getElementById('root'))
