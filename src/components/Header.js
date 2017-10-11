15254e5a3876c744c824cfe2403ea2891b29e96d
karanagarwal17 <karanagarwal1702@gmail.com>
2017-10-11 16:35:15 +0530

    changed the name

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <div className="logo">Socially</div>
+        <div className="logo">Helping Hands</div>
         <button className="btn logout-button" onClick={this.props.onLogout}>
           LOGOUT
         </button>
