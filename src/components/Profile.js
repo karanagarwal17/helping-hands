3ac6866eb88b2901f9d529a47fb5bb6ece2cc2f7
Meetasha29 <meetaasha29@gmail.com>
2017-10-27 16:29:10 +0530

    style button and add details

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,7 +15,19 @@ class Profile extends React.Component {
               <img src="img/background.jpg" alt="Profile icon" className="profileImage"/>
             </div>
             <div className="col span-2-of-3 detailsContainer ">
+            <div className="detailTitle">
               Meetasha Gaur
+            </div>
+            <div className="details">
+              <ul>
+                <li>Student</li><br/>
+                <li className="email">Meetaasha29@gmail.com</li>
+                <li>Jaipur 9782976966</li>
+              </ul>
+               <button class="button">Followers!</button> 
+
+            </div>
+
             </div>
         </div>
       </div>
