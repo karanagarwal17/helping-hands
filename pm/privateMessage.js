636f14ebdc2caad55c2bad8a20c63438e991b27d
karanagarwal17 <karanagarwal1702@gmail.com>
2017-10-31 11:32:11 +0530

    updated the chat

--- pm/privateMessage.js
+++ pm/privateMessage.js
@@ -2,5 +2,11 @@ module.exports=function(io){
 
   io.on("connection",function(socket){
     console.log("connected to socket successfully");
+
+    socket.on('send message', function(data){
+      console.log(data);
+
+      io.emit('receive message',data);
+    })
   })
 }
--- src/components/Chat/ChatApp.js
+++ src/components/Chat/ChatApp.js
@@ -1,67 +1,70 @@
 import React from 'react'
 import openSocket from 'socket.io-client'
 
+import Message from './Message'
+
 class ChatApp extends React.Component {
-  constructor() {
+  constructor(props) {
     super()
     this.state = {
-      response: false,
-      endpoint: "http://localhost:3000"
+      input: '',
+      messages: [
+        {
+          reply: true,
+          text: "first message!"
+        }, {
+          reply: false,
+          text: "second!"
+        }
+      ]
     }
 
-    this.addUsername = function(x) {
-      var element = document.getElementById("chat-messages");
-      var html = element.innerHTML;
-      html += "<label class=\"left\">User: " + x + "</label>";
-      element.innerHTML = html;
-      element.scrollTop = element.scrollHeight;
-    }
-
-    // var sendMessage = function() {
-    //   var msg = document.getElementById("message-content").value;
-    //   document.getElementById("message-content").value = "";
-    //   if (msg !== "") {
-    //     socket.emit('new message', {
-    //       roomId: id,
-    //       message: msg,
-    //       username: username
-    //     });
-    //   }
-    // }
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.handleOnSubmit = this.handleOnSubmit.bind(this)
+    this._handleMessageEvent = this._handleMessageEvent.bind(this)
   }
 
   componentDidMount() {
-    const { endpoint } = this.state
-    const socket = openSocket(endpoint)
+    this.socket = openSocket('http://localhost:3000')
+    this._handleMessageEvent()
+  }
 
-    socket.on('message created', function(data) {
-      console.log(data);
-      if (data.username == username) {
-        addMessage(data.message);
-      } else {
-        addUsername(data.username);
-        addReply(data.message);
-      }
-    });
+  _handleMessageEvent() {
+    this.socket.on('receive message', (inboundMessage) => {
+      console.log("inboundMessage");
+      var messages = this.state.messages
+      messages.push({text: inboundMessage.message, reply: true})
+      this.setState(messages: messages)
+    })
+  }
 
-    socket.on('setup', function(data) {
-      socket.emit('new user', {
-        roomId: id,
-        username: username
-      });
-    });
+  handleOnChange(ev) {
+    this.setState({input: ev.target.value})
+  }
 
+  handleOnSubmit(ev) {
+    ev.preventDefault()
+    this.socket.emit('send message', {message: this.state.input})
+    var messages = this.state.messages
+    messages.push({text: this.state.input, reply: false})
+    this.setState({messages: messages})
+    this.setState({input: ''})
   }
 
   render() {
     return (
       <div className="chat-container">
-        <div id="chat-messages">
+        <div className="chat-messages">
           <label>Chat</label>
+          {
+            this.state.messages.map((message, i) => {
+              return (<Message key={i} reply={message.reply} text={message.text}/>)
+            })
+          }
         </div>
-        <div id="sendmessage">
-          <input type="text" placeholder="Send message..." id="message-content"/>
-          <button type={'submit'} id="send" onclick="sendMessage()"></button>
+        <div className="sendmessage">
+          <input type="text" placeholder="Send message..." onChange={this.handleOnChange} value={this.state.input}/>
+          <button type="submit" id="send" onClick={this.handleOnSubmit}></button>
         </div>
       </div>
     )
