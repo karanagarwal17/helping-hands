1a1f8d200a6de59b507ef1d63bbe90760394a109
karanagarwal17 <karanagarwal1702@gmail.com>
2017-11-03 15:00:59 +0530

    completed the search

---
+++ src/components/Search/index.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import agent from '../../agent'
+import { Link } from 'react-router'
+
+import Header from '../Header'
+import Dashboard from '../Dashboard'
+import NgoDashboard from '../NgoDashboard'
+import NgoCard from '../Profile/NgoCard'
+import EventCard from '../Profile/EventCard'
+import VolunteerCard from '../Profile/VolunteerCard'
+import { SEARCH } from '../../constants/actionTypes'
+
+const mapStateToProps = state => ({
+  ...state.search,
+  currentUser: state.common.currentUser,
+  searchResults: state.search.searchResults || []
+})
+
+const mapDispatchToProps = dispatch => ({
+  onSearch: (type, query) =>
+    dispatch({ type: SEARCH, payload: agent.Search.post({type: type, name: query})})
+})
+
+class Search extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      query: '',
+      type: ''
+    }
+
+    this.updateState = field => ev => {
+      const state = this.state
+      const newState = Object.assign({}, state, {[field]: ev.target.value})
+      this.setState(newState)
+    }
+
+    this.updateType = ev => {
+      this.setState(Object.assign({}, this.state, {type: ev.target.value}))
+    }
+
+    this.search = ev => {
+      ev.preventDefault()
+      this.props.onSearch(this.state.type,this.state.query)
+    }
+  }
+
+  render() {
+    if(this.props.currentUser){
+      if(this.props.currentUser.volunteer){
+        return (
+          <div>
+            <Header/>
+            <div className="row Profile">
+              <div className="col span-1-of-4">
+                <Dashboard active="search"/>
+              </div>
+              <div className="col span-3-of-4 searchContainer">
+                <div className="row">
+                  <div className="col span-7-of-8 searchBox">
+                    <input id="query" type="text" placeholder="Search" value={this.state.query} onChange={this.updateState('query')}></input>
+                  </div>
+                  <div className="col span-1-of-8 searchButton">
+                    <button className="search" onClick={this.search}>Search</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col span-3-of-4">
+                <div className="radio-field">
+                  <input id="option1" className="input" name="select" type="radio" value="volunteer" onChange={this.updateType}/>
+                  <label>Volunteer</label>
+                  <input id="option2" className="input" name="select" type="radio" value="ngo" onChange={this.updateType}/>
+                  <label>NGO</label>
+                  <input id="option3" className="input" name="select" type="radio" value="event" onChange={this.updateType}/>
+                  <label>Event</label>
+                </div>
+              </div>
+              <div className="col span-3-of-4 searchResults box">
+                {
+                  this.props.searchResults.map((result,key) => {
+                    if(result.org_name){
+                      return (
+                        <NgoCard key={key} data={result}/>
+                      )
+                    } else if(result.firstname) {
+                      return(
+                        <VolunteerCard key={key} data={result}/>
+                      )
+                    } else {
+                      return(
+                        <EventCard key={key} data={result}/>
+                      )
+                    }
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            <Header/>
+            <div className="row Profile">
+              <div className="col span-1-of-4">
+                <NgoDashboard active="search"/>
+              </div>
+              <div className="col span-3-of-4 searchContainer">
+                <div className="row">
+                  <div className="col span-7-of-8 searchBox">
+                    <input id="query" type="text" placeholder="Search" value={this.state.query} onChange={this.updateState('query')}></input>
+                  </div>
+                  <div className="col span-1-of-8 searchButton">
+                    <button className="search" onClick={this.search}>Search</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col span-3-of-4">
+                <div className="radio-field">
+                  <input id="option1" className="input" name="select" type="radio" value="volunteer" onChange={this.updateType}/>
+                  <label>Volunteer</label>
+                  <input id="option2" className="input" name="select" type="radio" value="ngo" onChange={this.updateType}/>
+                  <label>NGO</label>
+                  <input id="option3" className="input" name="select" type="radio" value="event" onChange={this.updateType}/>
+                  <label>Event</label>
+                </div>
+              </div>
+              <div className="col span-3-of-4 searchResults box">
+                {
+                  this.props.searchResults.map((result,key) => {
+                    if(result.org_name){
+                      return (
+                        <NgoCard key={key} data={result}/>
+                      )
+                    } else if(result.firstname) {
+                      return(
+                        <VolunteerCard key={key} data={result}/>
+                      )
+                    } else {
+                      return(
+                        <EventCard key={key} data={result}/>
+                      )
+                    }
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        )
+      }
+    } else {
+      return (
+        <div>
+          Error
+        </div>
+      )
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search)
